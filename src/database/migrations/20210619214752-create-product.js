63892655007cc5f55c6d02879a5a01dd0b05cd51
Mabylon <84751718+MiguelAbal14@users.noreply.github.com>
2021-06-20 23:10:10 -0300

    Arreglo llave foranea

--- src/database/migrations/20210619214752-create-product.js
+++ src/database/migrations/20210619214752-create-product.js
@@ -15,19 +15,40 @@ module.exports = {
         type: Sequelize.DECIMAL
       },
       brand_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+          type: Sequelize.INTEGER,
+        references: {
+          model: 'brands',
+          key: 'id'
+        }
       },
       category_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+         references: {
+          model: 'categories',
+          key: 'id'
+        }
       },
       size_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+         references: {
+          model: 'sizes',
+          key: 'id'
+        }
       },
       gender_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+          references: {
+          model: 'genders',
+          key: 'id'
+        }
       },
       image_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+           references: {
+          model: 'images',
+          key: 'id'
+        }
       },
       createdAt: {
         allowNull: false,
